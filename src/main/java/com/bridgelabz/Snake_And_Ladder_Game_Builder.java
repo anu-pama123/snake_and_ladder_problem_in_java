ce2301bb30c93e60299bbc5c1aac119bfa0349e3
anupama cv <anu.anupamacv@gmail.com>
2021-05-20 23:27:29 +0530

    ADDED: functionality to get random numbers when rolling die

--- src/main/java/com/bridgelabz/Snake_And_Ladder_Game_Builder.java
+++ src/main/java/com/bridgelabz/Snake_And_Ladder_Game_Builder.java
@@ -1,8 +1,14 @@
 package com.bridgelabz;
 
 public class Snake_And_Ladder_Game_Builder {
-    public static void main(String[] arg)
-    {
-        System.out.println("Snake and ladder game at start position 0 ");
+    public static void main(String[] arg) {
+        int random_number=0;
+        while (1==1) {
+            random_number = (int) (Math.floor((Math.random() * 10)) % 7);
+            if(random_number!=0){
+                break;
+            }
+        }
+        System.out.println("Die number  " + random_number);
     }
 }
