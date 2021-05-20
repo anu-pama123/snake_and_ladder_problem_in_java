61a1859cc301aefb73f3ff6534916ba00f5dee35
anupama cv <anu.anupamacv@gmail.com>
2021-05-20 20:09:06 +0530

    ADDED: welcome message

---
+++ src/main/java/com/bridgelabz/Snake_And_Ladder_Game_Builder.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class Snake_And_Ladder_Game_Builder {
+    private static void main(String[] arg)
+    {
+        System.out.println("Welcome to snake and ladder game");
+    }
+}
