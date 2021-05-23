045b4399a02c73a930dc9af53250f0f524ad33ee
anupama cv <anu.anupamacv@gmail.com>
2021-05-23 19:54:40 +0530

    Refactored while loop to get number of times dice played to win the game

--- src/main/java/com/bridgelabz/Snake_And_Ladder_Game_Builder.java
+++ src/main/java/com/bridgelabz/Snake_And_Ladder_Game_Builder.java
@@ -7,6 +7,7 @@ public class Snake_And_Ladder_Game_Builder
         int random_number = 0;
         int total = 0;
         int myId, myValue, myDict;
+        int no_of_times=0;
         Hashtable<String, String> my_dict = new Hashtable<String, String>();
         my_dict.put("6", "-3");
         my_dict.put("10", "36");
@@ -14,7 +15,14 @@ public class Snake_And_Ladder_Game_Builder
         my_dict.put("17", "80");
         while (true)
         {
+            no_of_times++;
             random_number = (int) (Math.floor((Math.random() * 10)) % 7);
+            if(random_number==0)
+            {
+                continue;
+            }
+            System.out.println("Dice number = " + random_number);
+
             if (total+random_number > 100)
             {
                 continue;
@@ -26,12 +34,14 @@ public class Snake_And_Ladder_Game_Builder
             {
                 total = (total + Integer.valueOf(my_dict.get(temp)));
             }
-            System.out.println(total);
+            System.out.println("Position = " + total);
             if (total >= 100)
             {
                 break;
             }
         }
+        System.out.println("Number of times the dice roll to win the game = " + String.valueOf(no_of_times));
+
 
     }
 }
