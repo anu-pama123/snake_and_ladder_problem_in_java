2656b3f177c4da95014484f012b5b08f3bf23496
anupama cv <anu.anupamacv@gmail.com>
2021-05-23 18:28:24 +0530

    ADDED: functionality to check no play,ladder or snake

--- src/main/java/com/bridgelabz/Snake_And_Ladder_Game_Builder.java
+++ src/main/java/com/bridgelabz/Snake_And_Ladder_Game_Builder.java
@@ -1,14 +1,53 @@
 package com.bridgelabz;
-
-public class Snake_And_Ladder_Game_Builder {
-    public static void main(String[] arg) {
-        int random_number=0;
-        while (1==1) {
-            random_number = (int) (Math.floor((Math.random() * 10)) % 7);
-            if(random_number!=0){
-                break;
-            }
+import java.util.*;
+public class Snake_And_Ladder_Game_Builder
+{
+    public static void main(String[] arg)
+    {
+        int no_of_times = 0;
+        int random_number = 0;
+        int total = 0;
+        int myId, myValue, myDict;
+        Hashtable<String, String> my_dict = new Hashtable<String, String>();
+        my_dict.put("6", "-3");
+        my_dict.put("10", "36");
+        my_dict.put("12", "-5");
+        my_dict.put("17", "80");
+        random_number = (int) (Math.floor((Math.random() * 10)) % 7);
+        System.out.println("random number=" + random_number);
+        total = (total + random_number);
+        String temp = String.valueOf(total);
+        boolean flag = my_dict.containsKey(temp);
+        if (flag == true)
+        {
+            total = (total + Integer.valueOf(my_dict.get(temp)));
         }
-        System.out.println("Die number  " + random_number);
+        System.out.println(total);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
